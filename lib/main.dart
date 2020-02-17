bf2912de020967e0679f7c3e6281b65a4d019b64
Ioana Alexandru <ioanaa.alexandru98@gmail.com>
2020-02-17 18:42:52 +0200

    AppCenter initial configurationn

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:acs_upb_mobile/module/settings/settings_page.dart';
+import 'package:appcenter/appcenter.dart';
+import 'package:appcenter_analytics/appcenter_analytics.dart';
+import 'package:appcenter_crashes/appcenter_crashes.dart';
 import 'package:dynamic_theme/dynamic_theme.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:preferences/preferences.dart';
@@ -15,8 +19,50 @@ main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _appSecret;
+  String _installId = 'Unknown';
+  bool _areAnalyticsEnabled = false, _areCrashesEnabled = false;
+
+  _MyAppState() {
+    final ios = defaultTargetPlatform == TargetPlatform.iOS;
+    _appSecret = kIsWeb
+        ? "a2fc9ff0-a0aa-4a25-80e1-c1fb24c2ae77"
+        : ios
+            ? "225a5fc5-e5f5-4c85-bffa-7ac30be2116c"
+            : "61321521-b6a4-4d1f-b9e6-42a02dfbb5f6";
+  }
+
+  @override
+  initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  initPlatformState() async {
+    await AppCenter.start(
+        _appSecret, [AppCenterAnalytics.id, AppCenterCrashes.id]);
+
+    if (!mounted) return;
+
+    var installId = await AppCenter.installId;
+
+    var areAnalyticsEnabled = await AppCenterAnalytics.isEnabled;
+    var areCrashesEnabled = await AppCenterCrashes.isEnabled;
+
+    setState(() {
+      _installId = installId;
+      _areAnalyticsEnabled = areAnalyticsEnabled;
+      _areCrashesEnabled = areCrashesEnabled;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DynamicTheme(
